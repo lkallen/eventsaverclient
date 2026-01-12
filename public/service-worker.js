7c57373a6c21d40edb3aab6ae4b93ed4d4df63ef
Lindsay Allen <159066551+lkallen@users.noreply.github.com>
2026-01-12 13:17:47 -0500

    service worker added for pwa

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+// public/service-worker.js
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
